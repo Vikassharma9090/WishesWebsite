634219f418b7cce3fdc9d7115c62892f6b85fe16
Vikas Sharma <Vikaskumarasharma06470@gmail.com>
2021-04-08 13:35:46 +0530

    my first commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDom from "react-dom";
+import './index.css'
+
+
+// let curDate = new Date(2021,4,7,4);
+let curDate = new Date();
+curDate =curDate.getHours();
+let greeting ="";
+const cssStyle={};
+
+if(curDate>=1 && curDate <12){
+  greeting="Good Morning";
+  cssStyle.color="Green";
+
+}else if(curDate>=12 && curDate <19){
+  greeting = "Good Afternoon";
+  cssStyle.color="Orange";
+
+}else{
+  greeting ="Good Night";
+  cssStyle.color="Black";
+}
+
+
+
+ReactDom.render(
+  <>
+  <div>
+  <h1>Hello Sir,<span style={cssStyle}>{greeting}</span></h1>
+  </div>
+    
+  </>,
+
+  document.getElementById("root")
+);
